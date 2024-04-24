880a3cf403a197ba6c4b09f52922c0e37c90dba0
SheikhIrtiza <sheikhirtiza444@gmail.com>
2024-04-24 19:50:53 +0530

    edit app.py

--- app.py
+++ app.py
@@ -35,8 +35,7 @@ def mark_facespresent(name):
 # Initialize MTCNN detector
 detector = mtcnn.MTCNN()
 
-# Initialize webcam
-cap = cv2.VideoCapture(0)  # Initialize cap here
+
 
 # Load known encodings and names
 irtiza_encodings = np.load('encodings\irtiza_encodings.npy')
@@ -56,52 +55,61 @@ known_names = irtiza_names + mateen_names
 facespresent = {}
 
 # Main Streamlit app
-st.title("Face Recognition App")
+# st.title("Face Recognition App")
+st.markdown("""
+    <h1 style='text-align: center; color: grey;'>
+        🌟 <b>Face Recognition App</b> 🌟
+    </h1>
+    """, unsafe_allow_html=True)
+# Load and display the logo image
+logo_path = 'image.png'  
+st.image(logo_path, use_column_width=True)
 
+
+if st.button('Start Face Recognition'):
+   cap = cv2.VideoCapture(0) 
 # Create a placeholder for the frame to be displayed
-frame_placeholder = st.empty()
+   frame_placeholder = st.empty()
 
 
 # Streamlit loop
-while True:
-    ret, frame = cap.read()
-    try:
-        check_image(frame)
-    except ValueError as e:
-        print(e)
-        continue
+   while True:
+        ret, frame = cap.read()
+        try:
+            check_image(frame)
+        except ValueError as e:
+            print(e)
+            continue
 
-    faces = detect_faces(detector, frame)
-    draw_facebox(frame, faces)
+        faces = detect_faces(detector, frame)
+        draw_facebox(frame, faces)
 
     # Recognize faces
-    for face in faces:
-        x, y, width, height = face['box']
-        face_img = extract_face(frame, (x, y, width, height))
-        face_encoding = face_recognition.face_encodings(face_img)
-        if face_encoding:
-            matches = face_recognition.compare_faces(known_encodings, face_encoding[0])
-            name = "Unknown"
-            if True in matches:
-                match_index = matches.index(True)
-                name = known_names[match_index]
-                # Check if facespresent is already marked for this person
-                if name not in facespresent:
-                    facespresent[name] = True
-                    # Mark facespresent
-                    mark_facespresent(name)
-                    st.write(f"{name} marked facespresent at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        for face in faces:
+            x, y, width, height = face['box']
+            face_img = extract_face(frame, (x, y, width, height))
+            face_encoding = face_recognition.face_encodings(face_img)
+            if face_encoding:
+                matches = face_recognition.compare_faces(known_encodings, face_encoding[0])
+                name = "Unknown"
+                if True in matches:
+                    match_index = matches.index(True)
+                    name = known_names[match_index]
+                    # Check if facespresent is already marked for this person
+                    if name not in facespresent:
+                        facespresent[name] = True
+                        # Mark facespresent
+                        mark_facespresent(name)
+                        st.write(f"{name} marked facespresent at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
-            cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
-        else:
-            cv2.putText(frame, "Unknown", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+                cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+            else:
+                cv2.putText(frame, "Unknown", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
-    # Update the placeholder with the new frame
-    frame_placeholder.image(frame, channels="BGR")
+        # Update the placeholder with the new frame
+        frame_placeholder.image(frame, channels="BGR")
 
-    
+        
 
-# Save facespresent dictionary to a file
-    np.save('facespresent.npy', facespresent)
-
-
+    # Save facespresent dictionary to a file
+        np.save('facespresent.npy', facespresent)
